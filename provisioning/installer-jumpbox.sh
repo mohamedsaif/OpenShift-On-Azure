fb270890245a9473de0dfebdf63c7681a5d0f977
Mohamed Saif <mohamed.saif@outlook.com>
2020-03-07 12:19:50 +0400

    Redisgning guide around 4.3 installation

--- provisioning/installer-jumpbox.sh
+++ provisioning/installer-jumpbox.sh
@@ -16,6 +16,7 @@ INSTALLER_PIP=$(az vm create \
     --admin-username localadmin \
     --ssh-key-values ~/.ssh/installer-box-rsa.pub \
     --query publicIpAddress -o tsv)
+
 export INSTALLER_PIP=$INSTALLER_PIP >> ~/.bashrc
 # If you have an existing jumpbox, just set the public publicIpAddress
 # INSTALLER_PIP=YOUR_IP
@@ -36,7 +37,7 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 # Adding Azure DNS server (to handle the private name resoultion)
 sudo chmod o+r /etc/resolv.conf
 
-# Edit the server name to use Azure's DNS server fixed IP 168.63.129.16 (press i to be in insert mode, then ESC and type :wq to save and exit)
+# Edit the DNS server name to use Azure's DNS server fixed IP 168.63.129.16 (press i to be in insert mode, then ESC and type :wq to save and exit)
 sudo vi /etc/resolv.conf
 
 # Login to Azure
